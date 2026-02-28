d643592911da0a583f1401aacc3df38decadbab3
ubed-cilok <bidpendikalmatuq@gmail.com>
2026-02-28 12:09:46 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'nilai-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Mun aya cache pake nu aya, mun euweuh nyambung ka internet
+      return response || fetch(event.request);
+    })
+  );
+});
